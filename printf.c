0531e1a337c2e93a886b1a22843a19154e89c97e
gbenga-MA <michaeladeosun@ymail.com>
2022-08-06 13:36:47 +0100

    task 0

--- printf.c
+++ printf.c
@@ -1,3 +1,13 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <stddef.h>
+/**
+ * _printf - recreates the printf function
+ * @format: string with format specifier
+ * Return: number of characters printed
+ */
 int _printf(const char *format, ...)
 {
 	if (format != NULL)
@@ -39,3 +49,4 @@ int _printf(const char *format, ...)
 		return (count);
 	}
 	return (-1);
+}
